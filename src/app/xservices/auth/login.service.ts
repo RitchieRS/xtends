964b4a4b9fb5cb059cd621f894f9386671c7a52c
ricardojacinto <ricarancs@gmail.com>
2021-11-01 11:39:17 -0600

    Avance

--- src/app/xservices/auth/login.service.ts
+++ src/app/xservices/auth/login.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserResponse,User } from 'src/app/xmodels/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  login(authData: User): Observable<any>{
+    return this.http.post<UserResponse>(`${environment.API_URL}`, authData).pipe(
+          map(( user :  UserResponse)=>{
+
+          })
+        );
+  };
+  logauth(): void{}
+  private readToken():void{}
+  private saveToken():void{}
+  private handeleError():void{}
+
+
+
 }
