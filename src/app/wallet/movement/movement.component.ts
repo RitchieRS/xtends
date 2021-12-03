967531e553a31ba571817c838cef2acf22beaccf
D34Iupick <d34.eric@gmail.com>
2021-12-02 19:02:53 -0600

    5to Avance Wallet

--- src/app/wallet/movement/movement.component.ts
+++ src/app/wallet/movement/movement.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovementComponent implements OnInit {
 
+  detallemovimientos =[
+    {
+      movimiento:'Autozone',
+      mision:'Promotoría',
+      fecha:'13.oct.2021',
+      operacion:'+',
+      monto:'250.00',
+      moneda:'mx',
+      estado:'liberado',
+      colorServicio:'#7fb73f'
+    },
+    {
+      movimiento:'Toks Delivery',
+      mision:'Mystery Shopper',
+      fecha:'10.oct.2021',
+      operacion:'+',
+      monto:'120.00',
+      moneda:'mx',
+      estado:'pago por liberar',
+      colorServicio:'#020c3a'
+    },
+    {
+      movimiento:'Pago IZZI',
+      mision:'Mystery Shopper',
+      fecha:'6.oct.2021',
+      operacion:'-',
+      monto:'120.00',
+      moneda:'mx',
+      estado:'pago de servicio',
+      colorServicio:'red'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {}
