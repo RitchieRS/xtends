3945f7c6435aa60fb76cad67d9016eeb95d7dc1f
ricardojacinto <ricarancs@gmail.com>
2022-01-09 21:04:41 -0600

    Cambios Mission service

--- src/app/components/modalacceptmission/dialogacceptmission/dialogacceptmission.component.ts
+++ src/app/components/modalacceptmission/dialogacceptmission/dialogacceptmission.component.ts
@@ -1,14 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { ReqMission } from 'src/app/xmodels/missions';
+import { MissionService } from 'src/app/xservices/mission/mission.service';
+import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-dialogacceptmission',
   templateUrl: './dialogacceptmission.component.html',
   styleUrls: ['./dialogacceptmission.component.scss'],
 })
 export class DialogacceptmissionComponent implements OnInit {
+  dataMission : ReqMission;
+  token: string;
+  message = '';
+  accepted = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private srvMission : MissionService,private modalController: ModalController) {
+    const idTienda = Number(localStorage.getItem('idTienda'));
+    const idProyecto = Number(localStorage.getItem('idProyecto'));
+    this.token = localStorage.getItem('token');
+    this.dataMission = {
+      "idTienda": idTienda,
+      "idProyecto": idProyecto
+    };
 
-  ngOnInit() {}
+   }
+
+  ngOnInit() {
+
+    
+  }
+
+  acceptMission(){
+    console.log(this.dataMission);
+  
+    this.srvMission.acceptMissionXTiendaProyecto(this.dataMission,this.token).subscribe((res) =>{
+      if(res){
+        this.message = res.resp.msg;
+        this.accepted =true;
+        
+      }
+  
+    })
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
+  }
+
+
 
 }
--- src/app/xservices/mission/mission.service.ts
+++ src/app/xservices/mission/mission.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { ReqMission,Mission} from 'src/app/xmodels/missions';
+import { ReqMission,Mission, ResMissionAccepted} from 'src/app/xmodels/missions';
 import { catchError, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
@@ -32,6 +32,27 @@ export class MissionService {
           catchError((err)=> this.handeleError(err))
     );
   };
+
+  acceptMissionXTiendaProyecto(request: ReqMission,token : string):Observable<ResMissionAccepted | void>{
+    console.log(request);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Accept': 'text/html',
+        'Content-Type': 'application/json',
+        'Authorization' : token
+      }),
+      responseType: 'json' as 'json'
+    };
+  
+    
+    return this.http.post<Mission>(`${environment.API_URL}missions/accept`,request ,httpOptions).pipe(
+          map(( res :  Mission)=>{
+            console.log(res);
+            return res;
+          }),
+          catchError((err)=> this.handeleError(err))
+    );
+  };
   private handeleError(err) : Observable<never>{
     let erroMessage = "An error has ocurred";
     if(err){
@@ -40,6 +61,14 @@ export class MissionService {
     window.alert(erroMessage);
     return throwError(erroMessage);
 
+  }
+
+
+  public keepMissionInfo(data : any):void{
+    console.log("Start save Data");
+    localStorage.setItem('idTienda',data.idTienda);
+    localStorage.setItem('idProyecto',data.idProyecto);
+  
   }
 
 }
