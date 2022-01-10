a2437a273b8825cf6bc38a5a4ec19c72f38458fb
D34Iupick <d34.eric@gmail.com>
2022-01-09 21:42:36 -0600

    1era parte segundo menu

--- src/app/components/header-two/header-two.component.ts
+++ src/app/components/header-two/header-two.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header-two',
   templateUrl: './header-two.component.html',
   styleUrls: ['./header-two.component.scss'],
 })
-export class HeaderTwoComponent implements OnInit {
+export class HeaderTwoComponent {
+
+  @Input() titulo = '';
 
   constructor() { }
 
-  ngOnInit() {}
+
 
 }
